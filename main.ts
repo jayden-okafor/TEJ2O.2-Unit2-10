0855728657eaac10fe9068e7f7fcfa9ec4f1ac5c
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-03-19 16:22:14 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,69 +5,89 @@
  * This program shows you the light level using the microbit
  */
 
+// clear screen
 basic.clearScreen()
 
+// variables
 let neopixelStrip: neopixel.Strip = null
 let lightLevel:number = null
 
 neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
 // turn all 4 pixels off
-neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.show()
+neopixelStrip.clear()
 
 input.onButtonPressed(Button.A, function() {
-
     lightLevel = input.lightLevel()
 
-    if (lightLevel > 52) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    if (lightLevel <= 52) {
+        // show light level
+        basic.showNumber(lightLevel)
 
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        // turn off pixels
+        neopixelStrip.clear()
         neopixelStrip.show()
     }
 
-    if (lightLevel > 104) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    if (lightLevel > 52 && lightLevel <= 104) {
+        // show light level
+        basic.showNumber(lightLevel)
+    
+        // show red for 2 seconds
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+        basic.pause(2000)
 
+        // turn off pixels
+        neopixelStrip.clear()
+        neopixelStrip.show()
+    }
+
+    if (lightLevel > 104 && lightLevel <= 156) {
+        // show light level
+        basic.showNumber(lightLevel)
+
+        // show red and yellow for 2 seconds
         neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
         neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
         neopixelStrip.show()
+        basic.pause(2000)
 
+        // turn off pixels
+        neopixelStrip.clear()
+        neopixelStrip.show()
     }
 
-    if (lightLevel > 156) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    if (lightLevel > 156 && lightLevel <= 208) {
+        // show light level
+        basic.showNumber(lightLevel)
 
+        // show red, yellow, and green for 2 seconds
         neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
         neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
         neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
         neopixelStrip.show()
+        basic.pause(2000)
+
+        // turn off pixels
+        neopixelStrip.clear()
+        neopixelStrip.show()
     }
 
     if (lightLevel > 208) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        // show light level
+        basic.showNumber(lightLevel)
 
+        // show red, yellow, green, and white for 2 seconds
         neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
         neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
         neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
         neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
         neopixelStrip.show()
+        basic.pause(2000)
 
+        // turn off pixels
+        neopixelStrip.clear()
+        neopixelStrip.show()
     }
 })
